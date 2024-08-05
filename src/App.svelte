2aa03d0f3a199a49d0940a4db5be3d02ee1bf537
Jeremie Legault <jeremie.legault@avanade.com>
2024-08-05 11:27:01 -0400

    two way binding

--- src/App.svelte
+++ src/App.svelte
@@ -23,7 +23,8 @@
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
 <!-- <button on:click={changeName}>Change Name</button> -->
-<input type="text" value={name} on:input={nameInput} />
+<!-- <input type="text" value={name} on:input={nameInput} /> -->
+<input type="text" bind:value={name} />
 
 <style>
 	h1 {
