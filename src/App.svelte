9d8f265a262e27b532d1bb15a873a6d5cd637733
Jeremie Legault <jeremie.legault@avanade.com>
2024-08-05 11:25:57 -0400

    binding input value to variable ddeclared above

--- src/App.svelte
+++ src/App.svelte
@@ -3,18 +3,27 @@
 	let age = 30;
 
 	$: uppercaseName = name.toUpperCase();
-
+	$: console.log(name);
+	$: if (name === "Jeremie Legault") {
+		age = 45;
+	}
 	function incrementAge() {
 		age += 1;
 	}
 	function changeName() {
 		name = "Jeremie Legault";
 	}
+
+	function nameInput(event) {
+		const enteredValue = event.target.value;
+		name = enteredValue;
+	}
 </script>
 
 <h1>Hello {uppercaseName}, my age is {age}!</h1>
 <button on:click={incrementAge}>Change Age</button>
-<button on:click={changeName}>Change Name</button>
+<!-- <button on:click={changeName}>Change Name</button> -->
+<input type="text" value={name} on:input={nameInput} />
 
 <style>
 	h1 {
