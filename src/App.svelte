08867ef0ac29a400b4f484b77a4a3f72d7aedb2a
Jeremie Legault <jeremie.legault@avanade.com>
2024-08-05 11:19:11 -0400

    add change reactive element

--- src/App.svelte
+++ src/App.svelte
@@ -1,17 +1,23 @@
 <script>
-	let name = 'Jeremie';
+	let name = "Jeremie";
 	let age = 30;
+
+	$: uppercaseName = name.toUpperCase();
 
 	function incrementAge() {
 		age += 1;
 	}
+	function changeName() {
+		name = "Jeremie Legault";
+	}
 </script>
+
+<h1>Hello {uppercaseName}, my age is {age}!</h1>
+<button on:click={incrementAge}>Change Age</button>
+<button on:click={changeName}>Change Name</button>
 
 <style>
 	h1 {
 		color: purple;
 	}
 </style>
-
-<h1>Hello {name}, my age is {age}!</h1>
-<button on:click="{incrementAge}">Change Age</button>
